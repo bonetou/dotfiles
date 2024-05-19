059fadf654a4c548d3d7abf1cc5917a258fa601d
Henrique Boneto <54125602+bonetou@users.noreply.github.com>
2024-05-19 18:37:30 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,6 @@
 xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --primary
 xrandr --output DP-3 --mode 1920x1080 --rate 90 --pos 1920x0
 
-workspace 1 output DP-3
-workspace 2 output eDP-1
-
 
 # Set cursor size
 echo "Xcursor.size: 24" | xrdb -merge
