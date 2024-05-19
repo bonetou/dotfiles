83a16f1b71a3534859c1ef8ef5286df4ee8e4ca8
Henrique Boneto <54125602+bonetou@users.noreply.github.com>
2024-05-19 17:44:01 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,6 +10,8 @@ for monitor in $connected_monitors; do
     if [ "$monitor" == "DP-3" ]; then
         xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --primary
         xrandr --output DP-3 --mode 1920x1080 --rate 90 --pos 1920x0
+        workspace 1 output DP-3
+        workspace 2 output eDP-1
     fi
 done
 
